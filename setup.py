3c1df1b48a5278a8e6cba7f112eb37cd60b27ac9
Kairav Pithadia <kairavpithadia13@gmail.com>
2020-10-07 10:48:20 +0530

    Updated setup.py for 0.1.2

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mergemypdf",
-    version="0.1.1",
+    version="0.1.2",
     author="kairavkkp",
     author_email="kairavpithadia13@gmail.com",
-    description="Merge Images and PDFs using CLI with customizations.",
+    description="Merge Images, EPubs and PDFs using CLI with customizations.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
@@ -24,7 +24,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
     ],
-    keywords="Merge-pdf PDF-CLI merge pdf merge-pdf-local offline-merge pdf-merge jpg-pdf-merge image-pdf-merge png-jpg-merge-pdf encrypted-pdf password-protected protected-pdf",
+    keywords="Merge-pdf PDF-CLI merge pdf merge-pdf-local offline-merge pdf-merge jpg-pdf-merge image-pdf-merge png-jpg-merge-pdf encrypted-pdf password-protected protected-pdf merge-epub epub-image epub-pdf pdf-epub image-epub",
 
     entry_points={
         "console_scripts": ['mergemypdf = merge_pdf.merge_pdf:main'],
